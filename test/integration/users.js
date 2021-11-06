63afe2aa7a0154d6ce44359139bf2f0be80d9c27
Karan Shetty <shettykaran2110@gmail.com>
2021-11-06 12:20:59 +0530

    Made profiles public

--- test/integration/users.js
+++ test/integration/users.js
@@ -9,4 +9,15 @@ contract('users', () => {
 
     assert.isOk(tx)
   })
+
+  it('get user', async () => {
+    const storage = await UserStorage.deployed()
+    const userId = 1
+
+    const userInfo = await storage.profiles.call(userId)
+
+    const username = web3.utils.toAscii(userInfo[1]).replace(/\u0000/g, '')
+
+    assert.equal(username, 'karan')
+  })
 })
--- test/unit/TestUserStorage.sol
+++ test/unit/TestUserStorage.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 import 'truffle/Assert.sol';
 import 'truffle/DeployedAddresses.sol';
