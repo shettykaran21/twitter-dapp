2cae900f45010d3ecc6a9c73209bc8b085303951
Karan Shetty <shettykaran2110@gmail.com>
2022-01-02 13:41:29 +0530

    Clean nextjs project

--- client/web3/provider.js
+++
@@ -1,7 +0,0 @@
-export const provider = () => {
-  if (typeof window.ethereum !== 'undefined') {
-    return window.ethereum
-  } else {
-    console.error('You need to install MetaMask for this app to work!')
-  }
-}
