f43f7e299d087d61cd7cd04e4eac10cefb31c88c
Karan Shetty <shettykaran2110@gmail.com>
2022-01-09 00:56:55 +0530

    Function to get interactive version of contract

--- client/web3/provider.js
+++ client/web3/provider.js
@@ -1,4 +1,5 @@
 import Web3 from 'web3'
+import contract from '@truffle/contract'
 
 const provider = () => {
   if (typeof web3 !== 'undefined') {
@@ -9,3 +10,10 @@ const provider = () => {
 }
 
 export const eth = new Web3(provider()).eth
+
+export const getInstance = (artifact) => {
+  const contractObj = contract(artifact)
+  contractObj.setProvider(provider())
+
+  return contractObj.deployed()
+}
