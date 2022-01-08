fd3f08eced8b6c14ebbadb88396348f9421e0c28
Karan Shetty <shettykaran2110@gmail.com>
2022-01-09 00:16:46 +0530

    Access to contracts in client side

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,15 +2,16 @@ import { useEffect } from 'react'
 import { Box } from '@mui/material'
 
 import { eth } from '../web3/provider'
+import UserStorage from '../web3/artifacts/UserStorage.json'
 
 const HomePage = () => {
   useEffect(async () => {
+    console.log(UserStorage)
+
     try {
       await window.ethereum.enable()
       const addresses = await eth.getAccounts()
-      console.log(addresses)
       const balance = await eth.getBalance(addresses[0])
-      console.log(balance)
     } catch (err) {
       console.log('User denied access to their ETH addresses 😢')
     }
