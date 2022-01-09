c528063d8658899d6991bdc0a1d11ef36d639cc5
Karan Shetty <shettykaran2110@gmail.com>
2022-01-09 19:49:17 +0530

    Redundant test function calls

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,7 @@
-import { createUser, getUserInfo } from '@web3/users'
-import { createTweet, getTweetInfo } from '@web3/tweets'
 import Layout from '@components/layout'
 import LandingScreen from '@components/landing-screen'
 
 const HomePage = () => {
-  const logUser = async () => {
-    const userInfo = await getUserInfo(1)
-    console.log(userInfo)
-  }
-
-  const createTestUser = async () => {
-    const tx = await createUser('test')
-    console.log(tx)
-  }
-
-  const logTweet = async () => {
-    const tweetInfo = await getTweetInfo(1)
-    console.log(tweetInfo)
-  }
-
-  const createUserTweet = async () => {
-    const tx = await createTweet('Hello world!')
-    console.log(tx)
-  }
-
   return (
     <Layout>
       <LandingScreen />
