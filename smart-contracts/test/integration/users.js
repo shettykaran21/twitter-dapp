4fd574e1c97c09e8ca58c9214792606c5608ee37
Karan Shetty <shettykaran2110@gmail.com>
2022-01-09 15:05:49 +0530

    Replaced deprecated web util functions with new ones

--- smart-contracts/test/integration/users.js
+++ smart-contracts/test/integration/users.js
@@ -9,7 +9,7 @@ contract('users', () => {
     const storage = await UserStorage.deployed()
 
     try {
-      const username = web3.utils.fromAscii('karan')
+      const username = web3.utils.asciiToHex('karan')
       await storage.createUser(username)
       assert.fail()
     } catch (err) {
@@ -20,7 +20,7 @@ contract('users', () => {
   it('can create user with controller', async () => {
     const controller = await UserController.deployed()
 
-    const username = web3.utils.fromAscii('karan')
+    const username = web3.utils.asciiToHex('karan')
     const tx = await controller.createUser(username)
 
     assert.isOk(tx)
@@ -32,7 +32,7 @@ contract('users', () => {
     const userId = 1
     const userInfo = await storage.profiles.call(userId)
 
-    const username = web3.utils.toAscii(userInfo[1]).replace(/\u0000/g, '')
+    const username = web3.utils.hexToAscii(userInfo[1]).replace(/\u0000/g, '')
 
     assert.equal(username, 'karan')
   })
