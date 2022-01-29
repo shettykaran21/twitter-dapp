cba7e5687a4c1f2c122da3eaf5eb0508a2e43216
Karan Shetty <shettykaran2110@gmail.com>
2022-01-29 15:23:38 +0530

    Config to deploy to mumbai polygon testnet

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -1,9 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+
+const mnemonic = fs.readFileSync('.secret').toString().trim()
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*',
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://polygon-mumbai.g.alchemy.com/v2/ltMOHK5GvAZ35-VTzN_5e877fAA_ExDV`
+        ),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   compilers: {
